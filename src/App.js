7b669ff8d5ba20340be0b86a09983140859d1e29
Tommy Wilczk <tommywilczek@utexas.edu>
2019-07-19 20:43:59 -0500

    Rearranged div

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React from 'react';
 export default class App extends React.Component {
     state = { foo: 'bar' };
     render() {
-      return <div className="hello">
-        Hello, world!
-      </div>;
+      return <div className="hello">Hello, world!</div>;
     }
 }
